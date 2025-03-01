85deaad6edd577a9fb4b5e689d24e0b115d79091
Furkan Gözükara <furkangozukara@gmail.com>
2025-03-01 15:09:00 +0300

    Update inference_video.py

--- inference_video.py
+++ inference_video.py
@@ -9,9 +9,11 @@ import warnings
 import _thread
 import skvideo.io
 from queue import Queue, Empty
-from model.pytorch_msssim import ssim_matlab
+import subprocess  # Newly added for ffmpeg process handling
+import time
 
 warnings.filterwarnings("ignore")
+
 
 def transferAudio(sourceVideo, targetVideo):
     import shutil
@@ -20,12 +22,9 @@ def transferAudio(sourceVideo, targetVideo):
 
     # split audio from original video file and store in "temp" directory
     if True:
-
         # clear old "temp" directory if it exits
         if os.path.isdir("temp"):
-            # remove temp directory
             shutil.rmtree("temp")
-        # create new "temp" directory
         os.makedirs("temp")
         # extract audio from video
         os.system('ffmpeg -y -i "{}" -c:a copy -vn {}'.format(sourceVideo, tempAudioFileName))
@@ -35,23 +34,21 @@ def transferAudio(sourceVideo, targetVideo):
     # combine audio file and new video file
     os.system('ffmpeg -y -i "{}" -i {} -c copy "{}"'.format(targetNoAudio, tempAudioFileName, targetVideo))
 
-    if os.path.getsize(targetVideo) == 0: # if ffmpeg failed to merge the video and audio together try converting the audio to aac
+    if os.path.getsize(targetVideo) == 0:  # if ffmpeg failed to merge the video and audio together try converting the audio to aac
         tempAudioFileName = "./temp/audio.m4a"
         os.system('ffmpeg -y -i "{}" -c:a aac -b:a 160k -vn {}'.format(sourceVideo, tempAudioFileName))
         os.system('ffmpeg -y -i "{}" -i {} -c copy "{}"'.format(targetNoAudio, tempAudioFileName, targetVideo))
-        if (os.path.getsize(targetVideo) == 0): # if aac is not supported by selected format
+        if (os.path.getsize(targetVideo) == 0):  # if aac is not supported by selected format
             os.rename(targetNoAudio, targetVideo)
             print("Audio transfer failed. Interpolated video will have no audio")
         else:
             print("Lossless audio transfer failed. Audio was transcoded to AAC (M4A) instead.")
-
-            # remove audio-less video
             os.remove(targetNoAudio)
     else:
         os.remove(targetNoAudio)
 
-    # remove temp directory
     shutil.rmtree("temp")
+
 
 parser = argparse.ArgumentParser(description='Interpolation for a pair of images')
 parser.add_argument('--video', dest='video', type=str, default=None)
@@ -75,7 +72,7 @@ if args.exp != 1:
 assert (not args.video is None or not args.img is None)
 if args.skip:
     print("skip flag is abandoned, please refer to issue #207.")
-if args.UHD and args.scale==1.0:
+if args.UHD and args.scale == 1.0:
     args.scale = 0.5
 assert args.scale in [0.25, 0.5, 1.0, 2.0, 4.0]
 if not args.img is None:
@@ -86,10 +83,11 @@ torch.set_grad_enabled(False)
 if torch.cuda.is_available():
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark = True
-    if(args.fp16):
+    if args.fp16:
         torch.set_default_tensor_type(torch.cuda.HalfTensor)
 
 from train_log.RIFE_HDv3 import Model
+
 model = Model()
 if not hasattr(model, 'version'):
     model.version = 0
@@ -110,7 +108,6 @@ if not args.video is None:
         fpsNotAssigned = False
     videogen = skvideo.io.vreader(args.video)
     lastframe = next(videogen)
-    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     video_path_wo_ext, ext = os.path.splitext(args.video)
     print('{}.{}, {} frames in total, {}FPS to {}FPS'.format(video_path_wo_ext, args.ext, tot_frame, fps, args.fps))
     if args.png == False and fpsNotAssigned == True:
@@ -123,12 +120,15 @@ else:
         if 'png' in f:
             videogen.append(f)
     tot_frame = len(videogen)
-    videogen.sort(key= lambda x:int(x[:-4]))
+    videogen.sort(key=lambda x: int(x[:-4]))
     lastframe = cv2.imread(os.path.join(args.img, videogen[0]), cv2.IMREAD_UNCHANGED)[:, :, ::-1].copy()
     videogen = videogen[1:]
+
 h, w, _ = lastframe.shape
 vid_out_name = None
-vid_out = None
+ffmpeg_process = None  # This will be used for video saving
+
+# Modified saving logic:
 if args.png:
     if not os.path.exists('vid_out'):
         os.mkdir('vid_out')
@@ -137,9 +137,29 @@ else:
         vid_out_name = args.output
     else:
         vid_out_name = '{}_{}X_{}fps.{}'.format(video_path_wo_ext, args.multi, int(np.round(args.fps)), args.ext)
-    vid_out = cv2.VideoWriter(vid_out_name, fourcc, args.fps, (w, h))
+    # Instead of cv2.VideoWriter, we create an FFmpeg subprocess that encodes raw frames to H.264.
+    ffmpeg_cmd = [
+        'ffmpeg', '-y',
+        '-f', 'rawvideo',
+        '-vcodec', 'rawvideo',
+        '-pix_fmt', 'bgr24',
+        '-s', f'{w}x{h}',
+        '-r', str(int(np.round(args.fps))),
+        '-i', '-',  # input from pipe
+        '-an',
+        '-vcodec', 'libx264',
+        '-profile:v', 'high',
+        '-level', '3.1',
+        '-preset', 'veryslow',
+        '-crf', '18',
+        '-pix_fmt', 'yuv420p',
+        '-x264-params', 'ref=4:cabac=1',
+        vid_out_name
+    ]
+    ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
 
-def clear_write_buffer(user_args, write_buffer):
+
+def clear_write_buffer(user_args, write_buffer, ffmpeg_proc=None):
     cnt = 0
     while True:
         item = write_buffer.get()
@@ -149,7 +169,10 @@ def clear_write_buffer(user_args, write_buffer):
             cv2.imwrite('vid_out/{:0>7d}.png'.format(cnt), item[:, :, ::-1])
             cnt += 1
         else:
-            vid_out.write(item[:, :, ::-1])
+            # Convert from RGB (model output) back to BGR, then write raw bytes to ffmpeg
+            frame_bgr = item[:, :, ::-1]
+            ffmpeg_proc.stdin.write(frame_bgr.tobytes())
+
 
 def build_read_buffer(user_args, read_buffer, videogen):
     try:
@@ -159,33 +182,36 @@ def build_read_buffer(user_args, read_buffer, videogen):
             if user_args.montage:
                 frame = frame[:, left: left + w]
             read_buffer.put(frame)
-    except:
+    except Exception:
         pass
     read_buffer.put(None)
 
-def make_inference(I0, I1, n):    
+
+def make_inference(I0, I1, n):
     global model
     if model.version >= 3.9:
         res = []
         for i in range(n):
-            res.append(model.inference(I0, I1, (i+1) * 1. / (n+1), args.scale))
+            res.append(model.inference(I0, I1, (i + 1) * 1. / (n + 1), args.scale))
         return res
     else:
         middle = model.inference(I0, I1, args.scale)
         if n == 1:
             return [middle]
-        first_half = make_inference(I0, middle, n=n//2)
-        second_half = make_inference(middle, I1, n=n//2)
-        if n%2:
+        first_half = make_inference(I0, middle, n=n // 2)
+        second_half = make_inference(middle, I1, n=n // 2)
+        if n % 2:
             return [*first_half, middle, *second_half]
         else:
             return [*first_half, *second_half]
 
+
 def pad_image(img):
-    if(args.fp16):
+    if args.fp16:
         return F.pad(img, padding).half()
     else:
         return F.pad(img, padding)
+
 
 if args.montage:
     left = w // 4
@@ -197,14 +223,17 @@ padding = (0, pw - w, 0, ph - h)
 pbar = tqdm(total=tot_frame)
 if args.montage:
     lastframe = lastframe[:, left: left + w]
+
 write_buffer = Queue(maxsize=500)
 read_buffer = Queue(maxsize=500)
-_thread.start_new_thread(build_read_buffer, (args, read_buffer, videogen))
-_thread.start_new_thread(clear_write_buffer, (args, write_buffer))
 
-I1 = torch.from_numpy(np.transpose(lastframe, (2,0,1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
+# Start the read and write threads.
+_thread.start_new_thread(build_read_buffer, (args, read_buffer, videogen))
+_thread.start_new_thread(clear_write_buffer, (args, write_buffer, None if args.png else ffmpeg_process))
+
+I1 = torch.from_numpy(np.transpose(lastframe, (2, 0, 1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
 I1 = pad_image(I1)
-temp = None # save lastframe when processing static frame
+temp = None  # save lastframe when processing static frame
 
 while True:
     if temp is not None:
@@ -215,7 +244,7 @@ while True:
     if frame is None:
         break
     I0 = I1
-    I1 = torch.from_numpy(np.transpose(frame, (2,0,1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
+    I1 = torch.from_numpy(np.transpose(frame, (2, 0, 1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
     I1 = pad_image(I1)
     I0_small = F.interpolate(I0, (32, 32), mode='bilinear', align_corners=False)
     I1_small = F.interpolate(I1, (32, 32), mode='bilinear', align_corners=False)
@@ -223,19 +252,19 @@ while True:
 
     break_flag = False
     if ssim > 0.996:
-        frame = read_buffer.get() # read a new frame
+        frame = read_buffer.get()  # read a new frame
         if frame is None:
             break_flag = True
             frame = lastframe
         else:
             temp = frame
-        I1 = torch.from_numpy(np.transpose(frame, (2,0,1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
+        I1 = torch.from_numpy(np.transpose(frame, (2, 0, 1))).to(device, non_blocking=True).unsqueeze(0).float() / 255.
         I1 = pad_image(I1)
         I1 = model.inference(I0, I1, scale=args.scale)
         I1_small = F.interpolate(I1, (32, 32), mode='bilinear', align_corners=False)
         ssim = ssim_matlab(I0_small[:, :3], I1_small[:, :3])
         frame = (I1[0] * 255).byte().cpu().numpy().transpose(1, 2, 0)[:h, :w]
-        
+
     if ssim < 0.2:
         output = []
         for i in range(args.multi - 1):
@@ -255,12 +284,12 @@ while True:
     if args.montage:
         write_buffer.put(np.concatenate((lastframe, lastframe), 1))
         for mid in output:
-            mid = (((mid[0] * 255.).byte().cpu().numpy().transpose(1, 2, 0)))
+            mid = ((mid[0] * 255.).byte().cpu().numpy().transpose(1, 2, 0))
             write_buffer.put(np.concatenate((lastframe, mid[:h, :w]), 1))
     else:
         write_buffer.put(lastframe)
         for mid in output:
-            mid = (((mid[0] * 255.).byte().cpu().numpy().transpose(1, 2, 0)))
+            mid = ((mid[0] * 255.).byte().cpu().numpy().transpose(1, 2, 0))
             write_buffer.put(mid[:h, :w])
     pbar.update(1)
     lastframe = frame
@@ -273,18 +302,20 @@ else:
     write_buffer.put(lastframe)
 write_buffer.put(None)
 
-import time
-while(not write_buffer.empty()):
+while not write_buffer.empty():
     time.sleep(0.1)
 pbar.close()
-if not vid_out is None:
-    vid_out.release()
 
-# move audio to new video file if appropriate
+# If we are writing a video (not PNG sequence), close the ffmpeg pipe and wait for the process to finish.
+if not args.png:
+    ffmpeg_process.stdin.close()
+    ffmpeg_process.wait()
+
+# Move audio to the new video file if appropriate.
 if args.png == False and fpsNotAssigned == True and not args.video is None:
     try:
         transferAudio(args.video, vid_out_name)
-    except:
+    except Exception:
         print("Audio transfer failed. Interpolated video will have no audio")
         targetNoAudio = os.path.splitext(vid_out_name)[0] + "_noaudio" + os.path.splitext(vid_out_name)[1]
         os.rename(targetNoAudio, vid_out_name)
